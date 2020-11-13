4916b26b884a0535a1f71a1c6e14f986c938664a
Daniel <daniel@technikamateur.de>
2020-11-13 19:45:44 +0100

    creating a 2D array with random values is possible

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-	printf("Hello World\n");
+// Feldgröße ist 32
+const int GAME_SIZE = 32;
+int gameBoard[32][32];
+
+void field_initializer()
+{
+	// fills fields with random numbers 0 = dead, 1 = alive
+	srand(time(0));
+	for (int i = 0; i < GAME_SIZE; i++)
+	{
+		for (int j = 0; j < GAME_SIZE; j++)
+		{
+			gameBoard[i][j] = rand() % 2;
+		}
+	}
+}
+
+int main()
+{
+	field_initializer();
+	for (int i = 0; i < GAME_SIZE; i++)
+	{
+		for (int j = 0; j < GAME_SIZE; j++)
+		{
+			printf("%d ", gameBoard[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
