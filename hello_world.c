811052661c2d9c6e9244e89a3f033a48847995c0
Daniel <daniel@technikamateur.de>
2020-11-19 18:32:00 +0100

    changed array size and datatype

--- hello_world.c
+++ hello_world.c
@@ -3,24 +3,29 @@
 #include <time.h>
 
 // game size ist 16
-// is there a way to create gameBoard with gameSize ?
-const int gameSize = 16;
-int gameBoard[16][16];
+#define N 16
+u_int8_t state_1[N+2][N+2];
+u_int8_t state_2[N+2][N+2];
+u_int8_t* state_in_p=&state_1;
+u_int8_t* state_out_p=&state_2;
+u_int8_t* state_tmp_p=NULL;
+
 
 void field_initializer()
 {
     // fills fields with random numbers 0 = dead, 1 = alive
     srand(time(0));
-    for (int i = 0; i < gameSize; i++)
+    for (int i = 1; i < N+1; i++)
     {
-        for (int j = 0; j < gameSize; j++)
+        for (int j = 1; j < N+1; j++)
         {
-            gameBoard[i][j] = rand() % 2;
+            state_1[i][j] = rand() % 2;
         }
     }
     return;
 }
 
+// needs update
 int calculate_next_gen_cell(int neighbours)
 {
     // cell will be dead
@@ -34,32 +39,33 @@ int calculate_next_gen_cell(int neighbours)
     }
 }
 
+// needs update
 void calculate_next_gen()
 {
-    int nextGen[gameSize][gameSize];
+    int nextGen[N][N];
 
     // i = row, j = column
-    for (int i = 0; i < gameSize; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < gameSize; j++)
+        for (int j = 0; j < N; j++)
         {
             // coi = Cell of Interest
-            int coi = gameBoard[i][j];
+            int coi = state_1[i][j];
             // create an array which contains all neighbours
             int neighbourCounter = 0;
 
             // coi is left, top edge
             if (!i && !j)
             {
-                if (gameBoard[0][1])
+                if (state_1[0][1])
                 {
                     neighbourCounter++;
                 }
-                else if (gameBoard[1][1])
+                else if (state_1[1][1])
                 {
                     neighbourCounter++;
                 }
-                else if (gameBoard[1][0])
+                else if (state_1[1][0])
                 {
                     neighbourCounter++;
                 }
@@ -74,13 +80,14 @@ int main()
 {
     field_initializer();
     // print 2D array
-    for (int i = 0; i < gameSize; i++)
+    for (int i = 0; i < N+2; i++)
     {
-        for (int j = 0; j < gameSize; j++)
+        for (int j = 0; j < N+2; j++)
         {
-            printf("%d ", gameBoard[i][j]);
+            printf("%d ", state_1[i][j]);
         }
         printf("\n");
     }
+    printf("Array size is: %lu\n", (sizeof(state_1) / sizeof(u_int8_t)) / (N+2));
     return 0;
 }
