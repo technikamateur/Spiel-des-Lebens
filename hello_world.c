cfb46a7473e636946ee4a9b1a67a859dbe6df976
Daniel <daniel@technikamateur.de>
2020-11-23 11:25:06 +0100

    now functional

--- hello_world.c
+++ hello_world.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-// game size ist 16
-#define N 16
+//game size ist 32
+#define N 32
+int repetitions = 3;
+
 u_int8_t state_1[N + 2][N + 2];
 u_int8_t state_2[N + 2][N + 2];
-u_int8_t *state_in_p = &state_1;
-u_int8_t *state_out_p = &state_2;
-u_int8_t *state_tmp_p = NULL;
 
 void field_initializer()
 {
-    // fills fields with random numbers 0 = dead, 1 = alive
+    //fills fields with random numbers 0 = dead, 1 = alive
     srand(time(0));
     for (int i = 1; i < N + 1; i++)
     {
@@ -24,17 +23,16 @@ void field_initializer()
     return;
 }
 
-// needs update
-void calculate_next_gen()
+void calculate_next_gen(u_int8_t (*state)[N + 2][N + 2], u_int8_t (*state_old)[N + 2][N + 2])
 {
-    // i = row, j = column
+    //i = row, j = column
     for (int i = 1; i < N + 1; i++)
     {
         for (int j = 1; j < N + 1; j++)
         {
-            // count up a number (8)
-            u_int8_t sum_of_8 = state_1[i - 1][j - 1] + state_1[i - 1][j] + state_1[i - 1][j + 1] + state_1[i][j - 1] + state_1[i][j + 1] + state_1[i + 1][j - 1] + state_1[i + 1][j] + state_1[i + 1][j + 1];
-            state_2[i][j] = (sum_of_8 == 3) || ((sum_of_8 == 2) && state_1[i][j]);
+            //count up a number (8)
+            u_int8_t sum_of_8 = (*state_old)[i - 1][j - 1] + (*state_old)[i - 1][j] + (*state_old)[i - 1][j + 1] + (*state_old)[i][j - 1] + (*state_old)[i][j + 1] + (*state_old)[i + 1][j - 1] + (*state_old)[i + 1][j] + (*state_old)[i + 1][j + 1];
+            (*state)[i][j] = (sum_of_8 == 3) || ((sum_of_8 == 2) && (*state_old)[i][j]);
         }
     }
     return;
@@ -43,7 +41,7 @@ void calculate_next_gen()
 int main()
 {
     field_initializer();
-    // print 2D array
+    //print 2D array
     for (int i = 0; i < N + 2; i++)
     {
         for (int j = 0; j < N + 2; j++)
@@ -53,16 +51,41 @@ int main()
         printf("\n");
     }
     printf("Array size is: %lu\n", (sizeof(state_1) / sizeof(u_int8_t)) / (N + 2));
-    printf("After one repetition:\n");
-    calculate_next_gen();
-    for (int i = 0; i < N + 2; i++)
+    printf("%d repetitions:\n", repetitions);
+    //calculation
+    for (int i = 0; i < repetitions; i++)
     {
-        for (int j = 0; j < N + 2; j++)
+        if (i & 1)
         {
-            printf("%d ", state_2[i][j]);
+            //ungerade
+            calculate_next_gen(&state_1, &state_2);
+            //print
+            for (int i = 0; i < N + 2; i++)
+            {
+                for (int j = 0; j < N + 2; j++)
+                {
+                    printf("%d ", state_1[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
         }
-        printf("\n");
+        else
+        {
+            //gerade oder 0
+            calculate_next_gen(&state_2, &state_1);
+            //print
+            for (int i = 0; i < N + 2; i++)
+            {
+                for (int j = 0; j < N + 2; j++)
+                {
+                    printf("%d ", state_2[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
     }
-    printf("done");
+    printf("Done.\n");
     exit(0);
 }
